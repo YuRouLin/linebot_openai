a3b400e04620b8075bd37d387cde499c75c95731
ccc5359 <101238588+ccc5359@users.noreply.github.com>
2024-06-03 14:20:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -54,7 +54,6 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    msg = event.message.text
 translations = {
     "幫我": "Help me",
     "改成": "Change to",
